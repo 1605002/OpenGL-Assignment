6a45cb21414b697cf65651a68220b944cd959a1e
1605002 <1605002@ugrad.cse.buet.ac.bd>
2021-03-20 13:00:34 +0600

    Problem 2 kisu ekta hoise

--- 1605002_2.cpp
+++ 1605002_2.cpp
@@ -1,31 +1,11 @@
-#include<bits/stdc++.h>
 #include <GL/glut.h>
+// If you are using Windows, replace GL/glut.h with glut.h
+
+#include<bits/stdc++.h>
 
 using namespace std;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-#define pi (2*acos(0.0))
-const int K = 5;
-const int SQ_LEN = 237;
-const int BIG_RAD = 84;
-const int SMALL_RAD = 10;
-const double CD = 0.05;
-const double EPS = 0;
-const int Z = 1;
-
-// Global variables
-double cameraHeight;
-double cameraAngle;
-int drawgrid;
-int drawaxes;
-double TD = 1;
-double angle;
-int chole;
-bool playing;
-uniform_real_distribution<double> dist(0,1);
-bool ekshathe[K+5][K+5];
-int zombie[K+5];
 
 struct point
 {
@@ -60,36 +40,35 @@ struct point
     }
 };
 
+
+// Constants declaration
+const double pi = 2*acos(0.0);
+const int K = 5; // Number of bubbles 
+const int SQ_LEN = 238; // Square length
+const int BIG_RAD = 84; // Big circle radius
+const int SMALL_RAD = 10; // Bubble radius
+const double MND = 0.001; // Minimum velocity co-efficient of bubbles
+const double MXD = 0.2; // Maximum velocity co-efficient of bubbles
+const double CD = 0.01; // Velocity co-efficient change in each UP/DOWN press
+const double EPS = 0;
+const int Z = 5; // Number of times a bubble will advance without checking anything after a collision
+const point RED(1, 0, 0);
+const point GREEN(0, 1, 0);
+const point CYAN(0.5, 1, 1);
+
+// Global variables
+double TD = 0.001; // Velocity co-efficient
+int chole; // Number of frames
+bool playing; // Is the game currently running or paused?
+uniform_real_distribution<double> dist(0,1);
+bool ekshathe[K+5][K+5]; // ekshathe[i][j] = true means i'th and j'th bubble entered the big circle while intersecting
+int zombie[K+5]; // Number of remaining times a bubble will advance without checking anything after a collision
+
 double durotto(const point& a, const point& b)
 {
 	double d = sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
 	return d;
 }
-
-struct Square
-{
-    double len;
-
-    void draw()
-    {
-        glColor3f(0, 1, 0);
-
-        glBegin(GL_LINES);{
-            glVertex3f(-len/2, len/2 , 0);
-            glVertex3f(len/2, len/2 , 0);
-
-            glVertex3f(len/2, len/2 , 0);
-            glVertex3f(len/2, -len/2 , 0);
-
-            glVertex3f(len/2, -len/2 , 0);
-            glVertex3f(-len/2, -len/2 , 0);
-
-            glVertex3f(-len/2, -len/2 , 0);
-            glVertex3f(-len/2, len/2 , 0);
-
-        }glEnd();
-    }
-} borgo;
 
 struct Circle
 {
@@ -99,18 +78,23 @@ struct Circle
     bool born;
     bool vitore;
 
+	// Determines if cicle c is completely inside this circle
     bool hasInside(const Circle& c)
     {
         double d = durotto(center, c.center);
         return d+EPS < radius-c.radius ;
     }
 
+	// Determines if circle c and this circle has any overlapping area
 	bool dhakka(const Circle& c)
 	{
 		double d = durotto(center, c.center);
 		return d <= radius+c.radius+EPS;
 	}
 
+	// Redirects this circle's velocity with respect to circle c
+	// Reflecs the velocity of this circle with respect to the common tangent
+	// Link for the formula: https://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
 	void redirect(const Circle& c)
 	{
 		point nrm = center-c.center;
@@ -119,7 +103,7 @@ struct Circle
 		velocity -= nrm*velocity.dot(nrm)*2;
 	}
 
-    void draw(point color) // 
+    void draw(point color)
     {
         point points[200];
         int segments = 100;
@@ -147,56 +131,34 @@ struct Circle
     }
 } brittos[K+5];
 
+// If circle c collides with the outer square, negate its associated velocity component
 void collideBorgo(Circle& c)
 {
-    if(c.center.x+c.radius >= borgo.len/2) c.velocity.x = -c.velocity.x;
-    if(c.center.x-c.radius <= -borgo.len/2) c.velocity.x = -c.velocity.x;
-    if(c.center.y+c.radius >= borgo.len/2) c.velocity.y = -c.velocity.y;
-    if(c.center.y-c.radius <= -borgo.len/2) c.velocity.y = -c.velocity.y;
+    if(c.center.x+c.radius >= SQ_LEN/2) c.velocity.x = -c.velocity.x;
+    if(c.center.x-c.radius <= -SQ_LEN/2) c.velocity.x = -c.velocity.x;
+    if(c.center.y+c.radius >= SQ_LEN/2) c.velocity.y = -c.velocity.y;
+    if(c.center.y-c.radius <= -SQ_LEN/2) c.velocity.y = -c.velocity.y;
 }
 
 
-void drawAxes()
+void drawSqaure()
 {
-	if(drawaxes==1)
-	{
-		glColor3f(1.0, 1.0, 1.0);
-		glBegin(GL_LINES);{
-			glVertex3f( 100,0,0);
-			glVertex3f(-100,0,0);
+	glColor3f(GREEN.x,GREEN.y,GREEN.z);
 
-			glVertex3f(0,-100,0);
-			glVertex3f(0, 100,0);
+	glBegin(GL_LINES);{
+		glVertex3f(-SQ_LEN/2, SQ_LEN/2 , 0);
+		glVertex3f(SQ_LEN/2, SQ_LEN/2 , 0);
 
-			glVertex3f(0,0, 100);
-			glVertex3f(0,0,-100);
-		}glEnd();
-	}
-}
+		glVertex3f(SQ_LEN/2, SQ_LEN/2 , 0);
+		glVertex3f(SQ_LEN/2, -SQ_LEN/2 , 0);
 
+		glVertex3f(SQ_LEN/2, -SQ_LEN/2 , 0);
+		glVertex3f(-SQ_LEN/2, -SQ_LEN/2 , 0);
 
-void drawGrid()
-{
-	int i;
-	if(drawgrid==1)
-	{
-		glColor3f(0.6, 0.6, 0.6);	//grey
-		glBegin(GL_LINES);{
-			for(i=-8;i<=8;i++){
+		glVertex3f(-SQ_LEN/2, -SQ_LEN/2 , 0);
+		glVertex3f(-SQ_LEN/2, SQ_LEN/2 , 0);
 
-				if(i==0)
-					continue;	//SKIP the MAIN axes
-
-				//lines parallel to Y-axis
-				glVertex3f(i*10, -90, 0);
-				glVertex3f(i*10,  90, 0);
-
-				//lines parallel to X-axis
-				glVertex3f(-90, i*10, 0);
-				glVertex3f( 90, i*10, 0);
-			}
-		}glEnd();
-	}
+	}glEnd();
 }
 
 void keyboardListener(unsigned char key, int x,int y){
@@ -213,10 +175,10 @@ void keyboardListener(unsigned char key, int x,int y){
 void specialKeyListener(int key, int x,int y){
 	switch(key){
 		case GLUT_KEY_DOWN:		//down arrow key
-            TD = max(0.2, TD-CD);
+            TD = max(MND, TD-CD);
 			break;
 		case GLUT_KEY_UP:		// up arrow key
-            TD = min(4.0, TD+CD);
+            TD = min(MXD, TD+CD);
 			break;
 		default:
 			break;
@@ -226,20 +188,6 @@ void specialKeyListener(int key, int x,int y){
 
 void mouseListener(int button, int state, int x, int y){	//x, y is the x-y of the screen (2D)
 	switch(button){
-		case GLUT_LEFT_BUTTON:
-			if(state == GLUT_DOWN){		// 2 times?? in ONE click? -- solution is checking DOWN or UP
-				drawaxes=1-drawaxes;
-			}
-			break;
-
-		case GLUT_RIGHT_BUTTON:
-			//........
-			break;
-
-		case GLUT_MIDDLE_BUTTON:
-			//........
-			break;
-
 		default:
 			break;
 	}
@@ -282,19 +230,16 @@ void display(){
 	****************************/
 	//add objects
 
-	drawAxes();
-	drawGrid();
-
     //glColor3f(1,0,0);
     //drawSquare(10);
 
     //drawSS();
-    borgo.draw();
+    drawSqaure();
 
-    brittos[0].draw(point(1,0,0));
+    brittos[0].draw(RED);
     for(int i = 1; i <= K; i++)
     {
-        if(brittos[i].born) brittos[i].draw(point(0.5, 1, 1));
+        if(brittos[i].born) brittos[i].draw(CYAN);
     }
 
 	//ADD this line in the end --- if you use double buffer (i.e. GL_DOUBLE)
@@ -312,6 +257,7 @@ void animate(){
         if(brittos[i].shuru == chole) brittos[i].born = true;
     }
 
+	// Moving the bubbles along the line of their velocity vectors
 	for(int i = 1; i <= K; i++)
 	{
 		if(brittos[i].born) brittos[i].center += brittos[i].velocity*TD;
@@ -321,10 +267,12 @@ void animate(){
 	{
 		if(brittos[i].born)
 		{
+			// If inside the big circle for the first time
 			if(brittos[0].hasInside(brittos[i]) && !brittos[i].vitore)
 			{
 				brittos[i].vitore = true;
 
+				// If right now it intersects with another bubble j, ekshathe[i][j] = ekshathe[j][i] = true
 				for(int j = 1; j <= K; j++)
 				{
 					if(i == j) continue;
@@ -339,6 +287,7 @@ void animate(){
 	{
 		for(int j = i+1; j <= K; j++)
 		{
+			// If two bubbles inside are not intersecting anymore, thier "ekshathe" is false
 			if(ekshathe[i][j] && !brittos[i].dhakka(brittos[j])) ekshathe[i][j] = ekshathe[j][i] = false;
 		}
 	}
@@ -349,7 +298,7 @@ void animate(){
 
 		if(brittos[i].vitore)
 		{
-			if(zombie[i] == 0)
+			if(zombie[i] == 0) // If it is ready for another collision
 			{
 				if(!brittos[0].hasInside(brittos[i]))
 				{
@@ -362,7 +311,7 @@ void animate(){
 				{
 					if(!brittos[j].vitore) continue;
 					if(ekshathe[i][j]) continue;
-					if(brittos[i].dhakka(brittos[j]) && zombie[j] == 0)
+					if(brittos[i].dhakka(brittos[j]) && zombie[j] == 0) // It is colliding with a bubble that is also ready for collision
 					{
 						brittos[i].redirect(brittos[j]);
 						brittos[j].redirect(brittos[i]);
@@ -384,27 +333,20 @@ void animate(){
 
 void init(){
 	//codes for initialization
-	drawgrid=0;
-	drawaxes=1;
-	cameraHeight=150.0;
-	cameraAngle=1.0;
-	angle=0;
     playing = true;
-
-    // Set center square length
-    borgo.len = SQ_LEN;
 
     brittos[0].radius = BIG_RAD, brittos[0].born = true, brittos[0].shuru = 0;
     for(int i = 1; i <= K; i++)
     {
         brittos[i].radius = SMALL_RAD;
-        brittos[i].center = point(-SQ_LEN/2+SMALL_RAD, -SQ_LEN/2+SMALL_RAD, 0);
-        brittos[i].shuru = i*250;
+        brittos[i].center = point(-SQ_LEN/2+SMALL_RAD, -SQ_LEN/2+SMALL_RAD, 0); // Lower left part of the square
+        brittos[i].shuru = i*5000;
         brittos[i].born = false;
         brittos[i].vitore = false;
 
-        double x = dist(rng); // Will be changed
-        double y = sqrt(1-x*x);
+        double x = dist(rng);
+		if(i == 3) x = 0.08;
+        double y = sqrt(1-x*x); // x^2 + y^2 = 1
         brittos[i].velocity = point(x, y, 0);
     }
 
@@ -434,7 +376,7 @@ int main(int argc, char **argv){
 	glutInitWindowPosition(0, 0);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);	//Depth, Double buffer, RGB color
 
-	glutCreateWindow("My OpenGL Program");
+	glutCreateWindow("Bubbles");
 
 	init();
 
